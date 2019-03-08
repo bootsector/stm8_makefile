a4c00e13608bb58a16cedcca17046f6ac9c6ddb7
Bruno Freitas <bruno@brunofreitas.com>
2019-03-08 19:18:53 -0300

    Shortened license header. Added Eclipse project files

--- delay.c
+++ delay.c
@@ -3,24 +3,8 @@
 
     Copyright (c) 2019 Bruno Freitas - http://www.brunofreitas.com/
 
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-*/
+    #include "LICENSE"
+ */
 
 #include "stm8s.h"
 #include "delay.h"
@@ -30,21 +14,21 @@
 static volatile uint16_t *TIM2_counter = (volatile uint16_t *) TIM2_CNTRH_ADDRESS;
 
 void delay_us(uint16_t us) {
-    // Prescaler = 2^1: Increments at every 1uS
-    TIM2_PSCR = 0x01; 
+	// Prescaler = 2^1: Increments at every 1uS
+	TIM2_PSCR = 0x01;
 
-    // Initialize counter
-    TIM2_EGR |= (1 << 1);
+	// Initialize counter
+	TIM2_EGR |= (1 << 1);
 
-    // Enable counter
-    TIM2_CR1 |= (1 << 1);
+	// Enable counter
+	TIM2_CR1 |= (1 << 1);
 
-    // Wait until counter reaches requested us
-    while(*TIM2_counter < us);
+	// Wait until counter reaches requested us
+	while(*TIM2_counter < us);
 }
 
 void delay_ms(uint16_t ms) {
-    for(uint16_t i = 0; i < ms; i++) {
-        delay_us(1000);
-    }
+	for(uint16_t i = 0; i < ms; i++) {
+		delay_us(1000);
+	}
 }
--- main.c
+++ main.c
@@ -3,30 +3,14 @@
 
     Copyright (c) 2019 Bruno Freitas - http://www.brunofreitas.com/
 
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-*/
+    #include "LICENSE"
+ */
 
 /*
     Multi source files Makefile based project template for STM8.
 
     Blink code based on https://lujji.github.io/blog/bare-metal-programming-stm8/
-*/
+ */
 
 #include "stm8s.h"
 #include "delay.h"
@@ -34,12 +18,12 @@
 #define LED_PIN     5
 
 void main() {
-    PB_DDR |= (1 << LED_PIN); // configure PD4 as output
-    PB_CR1 |= (1 << LED_PIN); // push-pull mode
-    
-    while (1) {
-        /* toggle pin every 1s */
-        PB_ODR ^= (1 << LED_PIN);
-        delay_ms(1000);
-    }
+	PB_DDR |= (1 << LED_PIN); // configure PD4 as output
+	PB_CR1 |= (1 << LED_PIN); // push-pull mode
+
+	while (1) {
+		/* toggle pin every 1s */
+		PB_ODR ^= (1 << LED_PIN);
+		delay_ms(1000);
+	}
 }
